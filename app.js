6f5724a984ecaa9c0305bf23db94a84063a98693
SaraToth <saratoth101@gmail.com>
2025-05-20 18:53:05 +0900

    Feat: Finish configuring app.js

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require("express");
+const { userInfo } = require("node:os");
 const app = express();
 const path = require("node:path");
+const usersRouter = require("./routes/usersRouter");
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.render("index", { title: "Home" });
-});
+//Allows us to use body in express-validator
+app.use(express.urlencoded({ extended: true }));
+app.use("/", usersRouter);
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log("My first app");
+    console.log("Node is watching");
 })
