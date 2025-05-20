f8b2f97c0353d2caa89614f8e3a3f95c205b27e9
SaraToth <saratoth101@gmail.com>
2025-05-20 19:01:17 +0900

    Feat: Convert route functions to controller fns

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,16 +1,12 @@
 const { Router } = require("express");
-
 const usersRouter = Router();
+const usersController = require("../controllers/usersController");
 
 usersRouter.get("/", (req, res) => {
     res.render("index", { title: "Home" });
 });
 
-usersRouter.get("/create", (req, res) => {
-    res.send("gets")
-});
-usersRouter.post("/create", (req, res) => {
-    res.send("posts");
-});
+usersRouter.get("/create", usersController.usersCreateGet);
+usersRouter.post("/create", usersController.usersCreatePost);
 
 module.exports = usersRouter;
